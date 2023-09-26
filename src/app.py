d9d011967205c558527d61a1aef0e11f297cc2b0
Kate Deras <65469487+quackate@users.noreply.github.com>
2023-09-26 14:37:17 +0000

    Endpoints Working, Authentication Added

--- src/app.py
+++ src/app.py
@@ -8,11 +8,19 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Favorites, Characters, Planets, Vehicles
 #from models import Person
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this "super secret" with something else!
+jwt = JWTManager(app)
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
@@ -36,14 +44,266 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+#Login/Register Endpoints
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route("/login", methods=["POST"])
+def login_user():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(username=username, password=password).first()
+    
+    if user is None :
+        return jsonify({"msg": "Wrong username or password"}), 401
 
-    return jsonify(response_body), 200
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
+
+@app.route('/register', methods=['POST'])
+def register_new_user():
+    if request.method == 'POST':
+        user = User()
+        user.username = request.get_json()['username']
+        user.firstname = request.get_json()['firstname']
+        user.lastname = request.get_json()['lastname']
+        user.email = request.get_json()['email']
+        user.password = request.get_json()['password']
+
+        db.session.add(user)
+        db.session.commit()
+
+        # Show the updated version of the favorites
+        users = []
+        db_result = User.query.all()
+        for item in db_result:
+            users.append(item.serialize())
+        return jsonify(users), 200
+    
+    return "Error Ocurred. Remember to add a username, firstname, lastname, email and password!", 404
+
+
+#Users Endpoints
+
+@app.route('/users', methods=['GET'])
+@jwt_required()
+def get_users():
+    if request.method == 'GET':
+        users = []
+        db_result = User.query.all()
+        for item in db_result:
+            users.append(item.serialize())
+        return jsonify(users), 200
+    
+    return "Invalid Method", 404
+
+@app.route('/users/favorites', methods=['GET'])
+@jwt_required()
+def get_user_favorites():
+    if request.method == 'GET':
+        favorites = []
+        db_result = Favorites.query.all()
+        for item in db_result:
+            favorites.append(item.serialize())
+        return jsonify(favorites), 200
+    
+    return "Invalid Method", 404
+
+#People/Characters Endpoints
+
+@app.route('/people', methods=['GET'])
+@jwt_required()
+def get_people():
+    if request.method == 'GET':
+        people = []
+        db_result = Characters.query.all()
+        for item in db_result:
+            people.append(item.serialize())
+        return jsonify(people), 200
+    
+    return "Invalid Method", 404
+
+@app.route('/people/<int:char_id>', methods=['GET'])
+@jwt_required()
+def get_people_id(char_id):
+    if request.method == 'GET':
+        people = []
+        db_result = Characters.query.filter_by(id=char_id).all()
+        for item in db_result:
+            people.append(item.serialize())
+        return jsonify(people), 200
+    
+    return "Invalid Method", 404
+
+@app.route('/favorites/people/<int:char_id>', methods=['POST'])
+@jwt_required()
+def add_new_favorite_character(char_id):
+    if request.method == 'POST':
+        favorite = Favorites()
+        favorite.user_id = get_jwt_identity()
+        favorite.char_id = char_id
+        favorite.type = 'characters'
+
+        db.session.add(favorite)
+        db.session.commit()
+
+        # Show the updated version of the favorites
+        favorites = []
+        db_result = Favorites.query.filter_by(user_id=get_jwt_identity())
+        for item in db_result:
+            favorites.append(item.serialize())
+        return jsonify(favorites), 200
+    
+    return "Invalid Method", 404
+
+@app.route('/favorites/people/<int:char_id>', methods=['DELETE'])
+@jwt_required()
+def delete_favorite_character(char_id):
+    if request.method == 'DELETE':
+        favorite = Favorites.query.filter_by(char_id=char_id, user_id=get_jwt_identity(), type='characters').first()
+
+        db.session.delete(favorite)
+        db.session.commit()
+
+        # Show the updated version of the favorites
+        favorites = []
+        db_result = Favorites.query.filter_by(user_id=get_jwt_identity())
+        for item in db_result:
+            favorites.append(item.serialize())
+        return jsonify(favorites), 200
+    
+    return "Invalid Method", 404
+
+
+#Planets Endpoints
+
+@app.route('/planets', methods=['GET'])
+@jwt_required()
+def get_planets():
+    if request.method == 'GET':
+        planets = []
+        db_result = Planets.query.all()
+        for item in db_result:
+            planets.append(item.serialize())
+        return jsonify(planets), 200
+    
+    return "Invalid Method", 404
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+@jwt_required()
+def get_planets_id(planet_id):
+    if request.method == 'GET':
+        planets = []
+        db_result = Planets.query.filter_by(id=planet_id).all()
+        for item in db_result:
+            planets.append(item.serialize())
+        return jsonify(planets), 200
+    
+    return "Invalid Method", 404
+
+@app.route('/favorites/planets/<int:planet_id>', methods=['POST'])
+@jwt_required()
+def add_new_favorite_planet(planet_id):
+    if request.method == 'POST':
+        favorite = Favorites()
+        favorite.user_id = get_jwt_identity()
+        favorite.planet_id = planet_id
+        favorite.type = 'planets'
+
+        db.session.add(favorite)
+        db.session.commit()
+
+        # Show the updated version of the favorites
+        favorites = []
+        db_result = Favorites.query.filter_by(user_id=get_jwt_identity())
+        for item in db_result:
+            favorites.append(item.serialize())
+        return jsonify(favorites), 200
+    
+    return "Invalid Method", 404
+
+@app.route('/favorites/planets/<int:planet_id>', methods=['DELETE'])
+@jwt_required()
+def delete_favorite_planet(planet_id):
+    if request.method == 'DELETE':
+        favorite = Favorites.query.filter_by(planet_id=planet_id, user_id=get_jwt_identity(), type='planets').first()
+
+        db.session.delete(favorite)
+        db.session.commit()
+
+        # Show the updated version of the favorites
+        favorites = []
+        db_result = Favorites.query.filter_by(user_id=get_jwt_identity())
+        for item in db_result:
+            favorites.append(item.serialize())
+        return jsonify(favorites), 200
+    
+    return "Invalid Method", 404
+
+
+#Vehicles Endpoints
+
+@app.route('/vehicles', methods=['GET'])
+@jwt_required()
+def get_vehicles():
+    if request.method == 'GET':
+        vehicles = []
+        db_result = Vehicles.query.all()
+        for item in db_result:
+            vehicles.append(item.serialize())
+        return jsonify(vehicles), 200
+    
+    return "Invalid Method", 404
+
+@app.route('/vehicles/<int:vehicle_id>', methods=['GET'])
+@jwt_required()
+def get_vehicles_id(vehicle_id):
+    if request.method == 'GET':
+        vehicles = []
+        db_result = Vehicles.query.filter_by(id=vehicle_id).all()
+        for item in db_result:
+            vehicles.append(item.serialize())
+        return jsonify(vehicles), 200
+    
+    return "Invalid Method", 404
+
+@app.route('/favorites/vehicles/<int:vehicle_id>', methods=['POST'])
+@jwt_required()
+def add_new_favorite_vehicle(vehicle_id):
+    if request.method == 'POST':
+        favorite = Favorites()
+        favorite.user_id = get_jwt_identity()
+        favorite.vehicle_id = vehicle_id
+        favorite.type = 'vehicles'
+
+        db.session.add(favorite)
+        db.session.commit()
+
+        # Show the updated version of the favorites
+        favorites = []
+        db_result = Favorites.query.filter_by(user_id=get_jwt_identity())
+        for item in db_result:
+            favorites.append(item.serialize())
+        return jsonify(favorites), 200
+    
+    return "Invalid Method", 404
+
+@app.route('/favorites/vehicles/<int:vehicle_id>', methods=['DELETE'])
+@jwt_required()
+def delete_favorite_vehicle(vehicle_id):
+    if request.method == 'DELETE':
+        favorite = Favorites.query.filter_by(vehicle_id=vehicle_id, user_id=get_jwt_identity(), type='vehicles').first()
+
+        db.session.delete(favorite)
+        db.session.commit()
+
+        # Show the updated version of the favorites
+        favorites = []
+        db_result = Favorites.query.filter_by(user_id=get_jwt_identity())
+        for item in db_result:
+            favorites.append(item.serialize())
+        return jsonify(favorites), 200
+    
+    return "Invalid Method", 404
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,17 +3,140 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__='user'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(250), nullable=False, unique=True)
+    firstname = db.Column(db.String(250), nullable=False)
+    lastname = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False, unique=True)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Users %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname
+        }
+
+class Favorites(db.Model):
+    __tablename__='favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    type = db.Column(db.Enum("characters", "planets", "vehicles", name="favorites_types"), nullable=False)
+    char_id = db.Column(db.Integer, db.ForeignKey('characters.id'), nullable=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=True)
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicles.id'), nullable=True)
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.user_id
+
+    def serialize(self):
+        if self.type == 'characters':
+            return {
+                "id": self.id,
+                "user_id": self.user_id,
+                "type": self.type,
+                "char_id": self.char_id,
+            }
+        elif self.type == 'planets':
+            return {
+                "id": self.id,
+                "user_id": self.user_id,
+                "type": self.type,
+                "planet_id": self.planet_id,
+            }
+        else: 
+            return {
+                "id": self.id,
+                "user_id": self.user_id,
+                "type": self.type,
+                "vehicle_id": self.vehicle_id,
+            }
+
+class Characters(db.Model):
+    __tablename__='characters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    hair_color = db.Column(db.String(250), nullable=True)
+    birth_year = db.Column(db.String(250), nullable=True)
+    gender = db.Column(db.String(250), nullable=False)
+    skin_color = db.Column(db.String(250), nullable=True)
+    eye_color = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Characters %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "hair_color": self.hair_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color
+        }
+
+class Planets(db.Model):
+    __tablename__='planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
+    rotation_period = db.Column(db.Integer, nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
+    population = db.Column(db.Integer, nullable=True)
+    climate = db.Column(db.String(250), nullable=False)
+    terrain = db.Column(db.String(250), nullable=True)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain
+        }
+    
+class Vehicles(db.Model):
+    __tablename__='vehicles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    model = db.Column(db.String(250), nullable=False)
+    vehicle_class = db.Column(db.String(250), nullable=False)
+    manufacturer = db.Column(db.String(250), nullable=False)
+    length = db.Column(db.Integer, nullable=False)  
+    crew = db.Column(db.Integer, nullable=False) 
+    cargo_capacity = db.Column(db.Integer, nullable=False) 
+
+    def __repr__(self):
+        return '<Vehicles %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "model": self.model,
+            "vehicle_class": self.vehicle_class,
+            "manufacturer": self.manufacturer,
+            "length": self.length,
+            "crew": self.crew,
+            "cargo_capacity": self.cargo_capacity
         }
